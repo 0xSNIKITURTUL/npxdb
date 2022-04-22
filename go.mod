a9ebac13286bec28914a0564d9dcb8eb8cc7008e
0xSNIKITURTUL <damishra.official@gmail.com>
2022-04-22 06:49:04 +0530

    feat: built data extraction and loading utility

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/gocolly/colly v1.2.0
 	github.com/jackc/pgx/v4 v4.15.0
+	github.com/joho/godotenv v1.4.0
 )
 
 require (
@@ -14,8 +15,8 @@ require (
 	github.com/antchfx/xmlquery v1.3.10 // indirect
 	github.com/antchfx/xpath v1.2.0 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
-	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.11.0 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -27,8 +28,9 @@ require (
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
-	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
-	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
-	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
+	golang.org/x/net v0.0.0-20220420153159-1850ba15e1be // indirect
+	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,17 +3,55 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 	"strings"
 	"sync"
 
 	"github.com/gocolly/colly"
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
+	_ "github.com/joho/godotenv/autoload"
 )
+
+const schema string = `DROP TABLE IF EXISTS public.characters;
+DROP TABLE IF EXISTS public.streamers;
+CREATE TABLE public.streamers (
+	id BIGSERIAL PRIMARY KEY,
+	username VARCHAR(32) UNIQUE NOT NULL,
+	pid VARCHAR(32) UNIQUE NOT NULL
+);
+CREATE TABLE public.characters (
+	id BIGSERIAL PRIMARY KEY,
+	fullname VARCHAR(255) NOT NULL,
+	fullname_token TSVECTOR NOT NULL,
+	player BIGINT NOT NULL REFERENCES public.streamers(id),
+	UNIQUE (fullname, player)
+);`
 
 var ctx = context.Background()
 
-func main() {}
+func main() {
+	pool, err := pgxpool.Connect(ctx, os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("error: %s", err.Error())
+		return
+	}
+	defer pool.Close()
+
+	streamers, characters := FetchData()
+
+	if err := ClearDB(pool); err != nil {
+		log.Fatalf("error: %s", err.Error())
+		return
+	}
+
+	if err := LoadStreamers(pool, streamers); err != nil {
+		log.Fatalf("error: %s", err.Error())
+		return
+	}
+
+	LoadCharacters(pool, characters)
+}
 
 func FetchData() ([][]interface{}, [][]string) {
 	var (
@@ -22,31 +60,39 @@ func FetchData() ([][]interface{}, [][]string) {
 	)
 
 	col := colly.NewCollector()
-	defer col.Visit("https://nopixel.hasroot.com/characters.php")
 	col.OnHTML("#characters", func(h *colly.HTMLElement) {
 		h.ForEach("div[data-streamer]", func(_ int, e *colly.HTMLElement) {
-			streamerID := strings.Trim(e.Attr("data-streamer"), " ")
-			characterName := strings.Trim(e.ChildText(".charName"), " ")
+			streamerID := e.Attr("data-streamer")
+			characterName := e.ChildText(".charName")
 			characterName = strings.ReplaceAll(characterName, "“”", "\"")
-			streamerName := strings.Trim(e.ChildText(".profileLink"), " ")
+			streamerName := e.ChildText(".profileLink")
 			streamerName = strings.ToLower(streamerName)
 
-			isPresent := false
+			isUnique := true
 			for _, streamer := range streamers {
 				if streamer[0] == streamerID {
-					isPresent = true
+					isUnique = false
 					break
 				}
 			}
-			if isPresent {
+			if isUnique {
 				streamers = append(streamers, []interface{}{streamerID, streamerName})
 			}
 
 			characters = append(characters, []string{characterName, streamerID})
 		})
 	})
+	col.Visit("https://nopixel.hasroot.com/characters.php")
+
+	defer log.Printf("Characters found: %d", len(characters))
+	defer log.Printf("Streamers found: %d", len(streamers))
 
 	return streamers, characters
+}
+
+func ClearDB(pool *pgxpool.Pool) error {
+	_, err := pool.Exec(ctx, schema)
+	return err
 }
 
 func LoadStreamers(pool *pgxpool.Pool, streamers [][]interface{}) error {
@@ -59,15 +105,15 @@ func LoadStreamers(pool *pgxpool.Pool, streamers [][]interface{}) error {
 	if err != nil {
 		return err
 	}
-	log.Printf("Streamers inserted: %d", count)
+	defer log.Printf("Streamers inserted: %d", count)
 	return nil
 }
 
-func LoadCharacters(pool *pgxpool.Pool, characters [][]string) error {
+func LoadCharacters(pool *pgxpool.Pool, characters [][]string) {
 	var wg sync.WaitGroup
 	count := 0
+	defer log.Printf("Characters inserted: %d", count)
 	defer wg.Wait()
-	errorChan := make(chan error)
 
 	for _, character := range characters {
 		wg.Add(1)
@@ -75,31 +121,23 @@ func LoadCharacters(pool *pgxpool.Pool, characters [][]string) error {
 			defer wg.Done()
 			conn, err := pool.Acquire(ctx)
 			if err != nil {
-				errorChan <- err
-				return
+				log.Printf("error: %s", err.Error())
 			}
 			defer conn.Release()
 
 			_, err = conn.Exec(
 				ctx,
-				`INSERT INTO TABLE public.characters
-				SELECT $1, to_tsvector($1), s.id
+				`INSERT INTO public.characters (fullname, fullname_token, player)
+				SELECT $1::VARCHAR, to_tsvector($1::VARCHAR), s.id
 				FROM public.streamers s
 				WHERE s.pid = $2;`,
 				character[0],
 				character[1],
 			)
 			if err != nil {
-				errorChan <- err
-				return
+				log.Printf("error: %s", err.Error())
 			}
 			count++
 		}(character, pool)
 	}
-
-	if err := <-errorChan; err != nil {
-		return err
-	}
-
-	return nil
 }
